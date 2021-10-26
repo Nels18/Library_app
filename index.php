cc6826d57db08adfd5009e9b4f3917779a704611
Nelson Belgarde <nelsonbelgarde@gmail.com>
2021-10-26 15:09:20 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang='fr'>
+<head>
+<meta charset='UTF-8'>
+<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+<meta http-equiv='X-UA-Compatible' content='ie=edge'>
+<meta name='description' content='Ma Bibliothèque'/>
+<link rel='stylesheet' href='style.css'>
+<title>Ma Bibliothèque</title>
+</head>
+<body>
+    <header>
+        <h1>Ma bibliothèque</h1>
+    </header>
+    <main>
+        <?php 
+        echo 'hello';
+        ?>
+    </main>
+</body>
+</html>
